9b8fdeefcbd00a49969fd5e09a199eee096b11d1
agiraldo <andres57706@gmail.com>
2018-10-22 07:47:06 -0500

    view parking status modified and integrated

--- src/routes/status.js
+++ src/routes/status.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('status', { title: 'Parking Status' });
+router.get('/', function (req, res, next) {
+  res.render('status', {
+    title: 'Parking Status',
+    sensorsCount: 3
+  });
 });
 
 module.exports = router;
